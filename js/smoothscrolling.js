49c202ac8fa950007fd7d4d786eaf715b4cee916
Asim21 <mianasim0121@gmail.com>
2016-07-03 18:18:45 +0500

    updated back-to-top section

--- js/smoothscrolling.js
+++ js/smoothscrolling.js
@@ -39,22 +39,22 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
 
 });
 
-$("#back-to-top a[href^='#']").on('click', function(e) {
-
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top -50
-     }, 700, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 700);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
 });
